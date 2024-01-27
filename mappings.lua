5c55715b86e916aec2f27af1ed2118efa13c30b7
sumdahl <sumirandahal46@gmail.com>
2024-01-27 19:36:44 +0545

    nvchad

---
+++ mappings.lua
@@ -0,0 +1,25 @@
+---@type MappingsTable
+local M = {}
+
+M.general = {
+  n = {
+    [";"] = { ":", "enter command mode", opts = { nowait = true } },
+    ["<leader>e"]= { "<cmd>NvimTreeToggle<cr>", "Toggle NvimTree"},
+
+    --  format with conform
+    ["<leader>fm"] = {
+      function()
+        require("conform").format()
+      end,
+      "formatting",
+    }
+
+  },
+  v = {
+    [">"] = { ">gv", "indent"},
+  },
+}
+
+-- more keybinds!
+
+return M
